41a23051d1a2784f915840fa5d5c32ba178113e7
Technophile <patricia.snodgrass@gmail.com>
2018-05-29 21:47:24 -0500

    updated files

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,11 @@ if (process.env.JAWSDB_URL) {
 
 } else {
   connection = mysql.createConnection({
-    host: "k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "z8d39ts0ace43gs0",
-    password: "io9ib07uaqcb4iy0",
-    database: "bcxlvkz3s9cg0u0p"
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
   });
 };
 
