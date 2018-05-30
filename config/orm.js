128367a629dcbccf60152395125c5e037eacb798
Technophile <patricia.snodgrass@gmail.com>
2018-05-29 21:25:40 -0500

    updated files

--- config/orm.js
+++ config/orm.js
@@ -17,19 +17,15 @@ function objToSql(ob) {
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
+
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
 
-  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
@@ -84,5 +80,4 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
 module.exports = orm;
