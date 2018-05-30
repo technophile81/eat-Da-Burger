b9bd92d808fea9f0a33479a6a17264b3f9cb92a9
Technophile <patricia.snodgrass@gmail.com>
2018-05-29 20:05:55 -0500

    updated files

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection;
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  connection = mysql.createConnection({
+    host: "k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "z8d39ts0ace43gs0",
+    password: "io9ib07uaqcb4iy0",
+    database: "bcxlvkz3s9cg0u0p"
+  });
+};
+
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
